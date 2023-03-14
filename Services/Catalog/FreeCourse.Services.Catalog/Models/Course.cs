4bd5b3946d1cf7a1c76fb42731f18ecba4d80d09
osman28tr <osmantonbul2001@gmail.com>
2023-03-14 23:32:38 +0300

    MongoDbDriver ile classlar oluşturuldu.(Catalog.API tarafı)

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string Picture { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+        public Feature Feature { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
