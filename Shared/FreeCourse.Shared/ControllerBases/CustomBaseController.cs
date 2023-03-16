ef0e76ee235aa1a490d07e56d492121ae276e4fd
osman28tr <osmantonbul2001@gmail.com>
2023-03-16 17:26:46 +0300

    Kurslar'a ait controller mekanizması oluşturuldu ve status kodu genericleştirildi.

---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,20 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController:ControllerBase
+    {
+        //bu sayede artık geri dönüş kodu genericleştirildi.
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
