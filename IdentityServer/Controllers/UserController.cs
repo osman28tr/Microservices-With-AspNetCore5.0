c74f9a8a0ecb1bac95e26e1589decf6082fb0875
osman28tr <osmantonbul2001@gmail.com>
2023-03-21 23:36:56 +0300

    IdentityServer tarafında kullanıcı kayıt endpoint'inin işlemi gerçekleştirildi.

---
+++ IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
