58dd474f5d0d7264d955485cf110774212458d7e
osman28tr <osmantonbul2001@gmail.com>
2023-04-25 18:05:53 +0300

    IdentityServer basket mikroservisinden haberdar edildi ve apiresourcelardaki ufak kelime hataları giderildi.

--- IdentityServer/Controllers/UserController.cs
+++ IdentityServer/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using FreeCourse.IdentityServer.Models;
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
 using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -18,5 +19,45 @@ namespace FreeCourse.IdentityServer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) return BadRequest();
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
+        }
     }
 }
