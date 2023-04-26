7517ae3754117d456c0901b959e7548f34649d4b
osman28tr <osmantonbul2001@gmail.com>
2023-04-26 13:09:44 +0300

    DiscountAPI mikroservisi oluşturuldu ve gerekli port ataması yapıldı.

--- Services/Basket/FreeCourse.Services.Basket/Controllers/BasketController.cs
+++ Services/Basket/FreeCourse.Services.Basket/Controllers/BasketController.cs
@@ -22,7 +22,6 @@ namespace FreeCourse.Services.Basket.Controllers
         [HttpGet]
         public async Task<IActionResult> GetBasket()
         {
-            var claims = User.Claims;
             return CreateActionResultInstance(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
         }   
         [HttpPost]
