9ff12f46c101b5e332f681158073c927e276349a
osman28tr <osmantonbul2001@gmail.com>
2023-05-04 13:17:17 +0300

    Gateway'de catalog mikroservis'i için yapılandırmalar yapıldı.

--- FreeCourse.Gateway/Program.cs
+++ FreeCourse.Gateway/Program.cs
@@ -17,8 +17,6 @@ namespace FreeCourse.Gateway
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-
-
             Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, config) =>
             {
                 config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
