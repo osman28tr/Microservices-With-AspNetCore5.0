c118503eb69ffd3fbb91ffb73b823c4988bcd4c8
osman28tr <osmantonbul2001@gmail.com>
2023-05-13 16:45:25 +0300

    SignIn metodu post işlemi gerçekleştirildi.

--- Frontends/FreeCourse.Web/Models/SignInInput.cs
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -4,10 +4,13 @@ namespace FreeCourse.Web.Models
 {
     public class SignInInput
     {
+        [Required]
         [Display(Name ="Email adresiniz:")]
         public string Email { get; set; }
+        [Required]
         [Display(Name = "Şifreniz:")]
         public string Password { get; set; }
+        [Required]
         [Display(Name = "Beni Hatırla:")]
         public bool IsRemember { get; set; }
     }
