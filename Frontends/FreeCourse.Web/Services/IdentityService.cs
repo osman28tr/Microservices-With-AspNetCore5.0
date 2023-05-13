82e9f1955fc513f7721ab8ddfb668fbca2b15307
osman28tr <osmantonbul2001@gmail.com>
2023-05-13 19:13:23 +0300

    Refresh token'ın revoke edilmesi işlemi gerçekleştirildi.

--- Frontends/FreeCourse.Web/Services/IdentityService.cs
+++ Frontends/FreeCourse.Web/Services/IdentityService.cs
@@ -73,16 +73,36 @@ namespace FreeCourse.Web.Services
 
             var properties = authenticationResult.Properties;
 
-            properties.StoreTokens(authenticationTokens);
+            properties.StoreTokens(authenticationTokens); //alınan yeni access token ile cookie güncellendi.
 
-            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, authenticationResult.Principal, properties);
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, authenticationResult.Principal, properties); //yeni bir cookie oluştu.(sıfırdan değil)
 
             return token;
         }
 
-        public Task RevokeRefreshToken()
+        public async Task RevokeRefreshToken()
         {
-            throw new System.NotImplementedException();
+            var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.BaseUri,
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            }); // identityserver'daki tüm endpointler'i verir.
+
+            if (disco.IsError)
+                throw disco.Exception;
+
+            var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            TokenRevocationRequest tokenRevocationRequest = new()
+            {
+                ClientId = _clientSettings.WebClientForUser.ClientId,
+                ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+                Address = disco.RevocationEndpoint,
+                Token = refreshToken,
+                TokenTypeHint = "refresh_token" //refresh token gönderildiğini belirtmek için.
+            };
+
+            await _httpClient.RevokeTokenAsync(tokenRevocationRequest);
         }
 
         public async Task<Response<bool>> SignIn(SignInInput signInInput)
