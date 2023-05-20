892a85bc3c836f489e8f544578c8fc6fc5572e5d
osman28tr <osmantonbul2001@gmail.com>
2023-05-20 15:10:46 +0300

    Client tarafında catalog mikroservisi ilgili işlemler için ICatalogService oluşturuldu.

---
+++ Frontends/FreeCourse.Web/Models/Catalog/CreateCourseInput.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCourse.Web.Models.Catalog
+{
+    public class CreateCourseInput
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Picture { get; set; }
+        public FeatureViewModel FeatureViewModel { get; set; }
+        public string CategoryId { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Web.Models.Catalog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseViewModel>> GetAllCourseAsync();
+        Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
+        Task<CourseViewModel> GetCourseById(string courseId);
+        Task<bool> CreateCourseAsync(CreateCourseInput createCourseInput);
+        Task<bool> UpdateCourseAsync(UpdateCourseInput updateCourseInput);
+        Task<bool> DeleteCourseAsync(string courseId);      
+    }
+}
