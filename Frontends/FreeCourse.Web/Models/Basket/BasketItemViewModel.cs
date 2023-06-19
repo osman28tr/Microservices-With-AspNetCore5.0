c27bda3588a57cbc2e11eb0b36222b931c5d2fd6
osman28tr <osmantonbul2001@gmail.com>
2023-06-19 12:21:56 +0300

    Basket mikroservisi için basketviewmodel ve basketitemviewmodel oluşturuldu.

---
+++ Frontends/FreeCourse.Web/Models/Basket/BasketItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FreeCourse.Web.Models.Basket
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+        private decimal? discountAppliedPrice { get; set; }
+
+        public decimal CurrentPrice { get => discountAppliedPrice != null ? discountAppliedPrice.Value : Price; }
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            discountAppliedPrice = discountPrice;
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Models/Basket/BasketViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Web.Models.Basket
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        private List<BasketItemViewModel> basketItems { get; set; }
+        public decimal TotalPrice { get => basketItems.Sum(x => x.CurrentPrice); }
+
+        public bool HasDiscount
+        {
+            get=> !string.IsNullOrEmpty(DiscountCode);
+        }
+
+        public List<BasketItemViewModel> BasketItems
+        {
+            get
+            {
+                if (HasDiscount)
+                {
+                    basketItems.ForEach(x =>
+                    {
+                        var discountPrice = x.Price * ((decimal)DiscountRate.Value / 100);
+                        x.AppliedDiscount(Math.Round(x.Price - discountPrice, 2));
+                    });
+                }
+                return basketItems;
+            }
+            set
+            {
+                basketItems = value;
+            }
+        }
+    }
+}
