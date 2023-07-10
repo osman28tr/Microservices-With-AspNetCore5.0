84b6e12fc15ec2479ed543522cbdc4ba8c4f4dfb
osman28tr <osmantonbul2001@gmail.com>
2023-07-10 18:28:24 +0300

    DiscountService oluşturuldu ve gerekli ayarlamalar yapıldı.

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IDiscountService.cs
@@ -0,0 +1,10 @@
+﻿using FreeCourse.Web.Models.Discount;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<DiscountViewModel> GetDiscount(string discountCode);
+    }
+}
