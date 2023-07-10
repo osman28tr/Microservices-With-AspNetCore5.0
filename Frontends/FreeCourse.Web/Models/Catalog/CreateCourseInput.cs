4532f8ad39ee960373d8c7db3660488f00c456e1
osman28tr <osmantonbul2001@gmail.com>
2023-07-10 17:38:17 +0300

    Kurs ekleme ve güncelleme işlemleri için fluentvalidation kütüphanesi kullanıldı.

--- Frontends/FreeCourse.Web/Models/Catalog/CreateCourseInput.cs
+++ Frontends/FreeCourse.Web/Models/Catalog/CreateCourseInput.cs
@@ -6,18 +6,17 @@ namespace FreeCourse.Web.Models.Catalog
     public class CreateCourseInput
     {
         [Display(Name = "Kurs ismi")]
-		[Required]
+
         public string Name { get; set; }
 		[Display(Name = "Kurs açıklama")]
-		[Required]
+	
 		public string Description { get; set; }
 		[Display(Name = "Kurs fiyat")]
-		[Required]
+		
 		public decimal Price { get; set; }
 		public string Picture { get; set; }
 		public FeatureViewModel Feature { get; set; }
 		[Display(Name = "Kategori")]
-		[Required]
 		public string CategoryId { get; set; }
         public string UserId { get; set; }
 		[Display(Name = "Kurs resmi")]
--- Frontends/FreeCourse.Web/Models/Catalog/UpdateCourseInput.cs
+++ Frontends/FreeCourse.Web/Models/Catalog/UpdateCourseInput.cs
@@ -8,17 +8,13 @@ namespace FreeCourse.Web.Models.Catalog
     {
         public string Id { get; set; }
 		[Display(Name = "Kurs ismi")]
-		[Required]
 		public string Name { get; set; }
 		[Display(Name = "Kurs açıklama")]
-		[Required]
 		public string Description { get; set; }
 		[Display(Name = "Kurs fiyat")]
-		[Required]
 		public decimal Price { get; set; }
         public FeatureViewModel Feature { get; set; }
 		[Display(Name = "Kategori")]
-		[Required]
 		public string CategoryId { get; set; }
         public string UserId { get; set; }
         public string Picture { get; set; }
