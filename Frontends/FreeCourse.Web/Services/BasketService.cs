73b118da6b9e5e928b9edf168006814cc38d6524
osman28tr <osmantonbul2001@gmail.com>
2023-07-10 19:03:23 +0300

    BasketService indirim işlemleri gerçekleştirildi.

--- Frontends/FreeCourse.Web/Services/BasketService.cs
+++ Frontends/FreeCourse.Web/Services/BasketService.cs
@@ -12,9 +12,11 @@ namespace FreeCourse.Web.Services
     public class BasketService : IBasketService
     {
         private readonly HttpClient _httpClient;
-        public BasketService(HttpClient httpClient)
+        private readonly IDiscountService _discountService;
+        public BasketService(HttpClient httpClient, IDiscountService discountService)
         {
             _httpClient = httpClient;
+            _discountService = discountService;
         }
         public async Task AddBasketItem(BasketItemViewModel basketItemViewModel)
         {
@@ -22,29 +24,55 @@ namespace FreeCourse.Web.Services
 
             if (basket != null)
             {
-                if(!basket.BasketItems.Any(x=>x.CourseId == basketItemViewModel.CourseId))
+                if (!basket.BasketItems.Any(x => x.CourseId == basketItemViewModel.CourseId))
                 {
                     basket.BasketItems.Add(basketItemViewModel);
                 }
             }
             else
-            {   
+            {
                 basket = new BasketViewModel();
-                
+
                 basket.BasketItems.Add(basketItemViewModel);
             }
 
             await SaveOrUpdate(basket);
         }
 
-        public Task<bool> ApplyDiscount(string discountCode)
+        public async Task<bool> ApplyDiscount(string discountCode)
         {
-            throw new System.NotImplementedException();
+            await CancelApplyDiscount();
+
+            var basket = await Get();
+
+            if (basket==null || basket.DiscountCode==null)
+            {
+                return false;
+            }
+
+            var hasDiscount = await _discountService.GetDiscount(discountCode);
+            if (hasDiscount==null)
+            {
+                return false;
+            }
+            basket.DiscountRate = hasDiscount.Rate;
+            basket.DiscountCode = hasDiscount.Code;
+            await SaveOrUpdate(basket);
+            return true;
         }
 
-        public Task<bool> CancelApplyDiscount()
+        public async Task<bool> CancelApplyDiscount()
         {
-            throw new System.NotImplementedException();
+            var basket = await Get();
+
+            if (basket == null || basket.DiscountCode == null)
+            {
+                return false;
+            }
+
+            basket.DiscountCode = null;
+            await SaveOrUpdate(basket);
+            return true;
         }
 
         public async Task<bool> Delete()
