2cc63c8a551845afe52063d478b1770aecb01cd6
osman28tr <osmantonbul2001@gmail.com>
2023-07-11 23:12:11 +0300

    order işlemleri için servis oluşturuldu ve gerekli model, http ayarlamaları yapıldı.

---
+++ Frontends/FreeCourse.Web/Models/Order/OrderCreatedViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourse.Web.Models.Order
+{
+    public class OrderCreatedViewModel
+    {
+        public int OrderId { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using FreeCourse.Web.Models.Order;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        //<summary>
+        //  senkron iletişim, order mikroservisine istek yapulır.
+        //</summary>
+        Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput);
+        //<summary>
+        //  asenkron iletişim, sipariş bilgileri rabbitMQ'ya gönderilecek.
+        //</summary>
+        Task Suspend(CheckoutInfoInput checkoutInfoInput);
+        Task<List<OrderViewModel>> GetOrder();
+    }
+}
